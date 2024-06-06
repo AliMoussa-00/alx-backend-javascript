75174da853ddf006e3098085f2f6981db74628d7
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-06-06 23:12:04 +0000

    fix 10..

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,31 +1,36 @@
 const express = require('express');
 
 const app = express();
-const port = 7865;
+const PORT = 7865;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.send(`Payment methods for cart ${req.params.id}`);
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const cartId = req.params.id;
+  res.send(`Payment methods for cart ${cartId}`);
 });
 
 app.get('/available_payments', (req, res) => {
-  const obj = { payment_methods: { credit_cards: true, paypal: false } };
-  res.json(obj);
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
 app.post('/login', (req, res) => {
   let username = '';
+
   if (req.body) {
     username = req.body.userName;
   }
-  res.end(`Welcome ${username}`);
+
+  res.send(`Welcome ${username}`);
 });
 
-app.listen(port, () => {
-  console.log('API available on localhost port 7865');
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
 
 module.exports = app;
+
